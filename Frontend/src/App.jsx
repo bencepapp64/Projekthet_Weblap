47fdb2e00fc3264fddc7652c8327b41c1539d9d4
szabodani04 <danielmark0401@gmail.com>
2023-02-06 15:16:13 +0100

    megnagyobbsemmi

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      
       <h1>LOGIN</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
