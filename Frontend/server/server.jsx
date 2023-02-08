919a8f6785ddbcca95d475eaa4096e6ca8e9a278
bencepapp64 <bence4436@gmail.com>
2023-02-08 19:56:09 +0100

    login panel működik

---
+++ Frontend/server/server.jsx
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const sqlite3 = require('sqlite3').verbose();
+
+app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader("Acces-Control-Allow-Origin", "*")
+    next()
+})
+app.use(express.json({limit:'10mb'}))
+
+let db = new sqlite3.Database('login.db', (err) => {
+    if (err) {
+        console.error(err.message)
+    }
+        console.log('Connected to DB')
+})
+
+app.post('/validatePassword', (req, res) => {
+    const {username , password} = req.body
+    
+    db.all(`select * from login where username = '${username}' and password = '${password}'` , (err, rows)=>{
+        if (err){
+            throw err
+        }
+        if (rows.length > 0){
+            res.send({validation: true})
+        }else{
+            res.send({validation: false})
+        }
+    })
+})
+
+app.listen(3001, ()=> console.log('Listening port: 3001'))
